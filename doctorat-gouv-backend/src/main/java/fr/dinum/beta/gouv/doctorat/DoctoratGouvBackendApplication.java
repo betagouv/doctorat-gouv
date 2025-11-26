1df1bb85ad5fe08c56c95ef9af526d3272df74d6
El-Hani Mohamed-Laarbi <mohamed-laarbi.el-hani@inetum.com>
2025-11-26 12:40:05 +0100

    Initial commit: setup backend (Spring Boot 4) + frontend (Angular 20)

---
+++ doctorat-gouv-backend/src/main/java/fr/dinum/beta/gouv/doctorat/DoctoratGouvBackendApplication.java
@@ -0,0 +1,14 @@
+package fr.dinum.beta.gouv.doctorat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DoctoratGouvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctoratGouvBackendApplication.class, args);
+	}
+
+}
---
+++ doctorat-gouv-backend/src/test/java/fr/dinum/beta/gouv/doctorat/DoctoratGouvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dinum.beta.gouv.doctorat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctoratGouvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
