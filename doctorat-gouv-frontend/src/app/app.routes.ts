505f6696a99cf771ebd4bcb7d9d4ce82225b0772
mohamed-beta-dev <mohamed-laarbi.el-hani.ext@beta.gouv.fr>
2025-12-10 11:30:50 +0100

    feat: Ajout d'une page home vide

--- doctorat-gouv-frontend/src/app/app.routes.ts
+++ doctorat-gouv-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Search } from './search/search';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Home },   // page d'accueil
+	{ path: 'home', component: Home }
+];
