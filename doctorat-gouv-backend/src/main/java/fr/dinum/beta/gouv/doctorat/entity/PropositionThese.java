a717888ee548bb236c648f852fe07f6c43426e0c
mohamed-beta-dev <mohamed-laarbi.el-hani.ext@beta.gouv.fr>
2025-12-15 10:29:05 +0100

    fix: Supression imports inutiles de PropositionThese

--- doctorat-gouv-backend/src/main/java/fr/dinum/beta/gouv/doctorat/entity/PropositionThese.java
+++ doctorat-gouv-backend/src/main/java/fr/dinum/beta/gouv/doctorat/entity/PropositionThese.java
@@ -13,13 +13,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "proposition_these")
