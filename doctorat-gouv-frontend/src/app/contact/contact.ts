b02114a3aca151a58a0037b216dca74541e684a1
mohamed-beta-dev <mohamed-laarbi.el-hani.ext@beta.gouv.fr>
2026-01-23 18:54:57 +0100

    fix: Fix bug nombre d'année >10

--- doctorat-gouv-frontend/src/app/contact/contact.ts
+++ doctorat-gouv-frontend/src/app/contact/contact.ts
@@ -39,7 +39,7 @@ export class Contact {
 	  'Autre'
 	];
 
-	annees = [1, 2, 3, 4, 5, 6, 7, 8, 9, '> 10'];
+	annees = [1, 2, 3, 4, 5, 6, 7, 8, 9, '10'];
 	secteurs = [
 		'Agriculture',
 		'Bâtiment - Travaux publics',
