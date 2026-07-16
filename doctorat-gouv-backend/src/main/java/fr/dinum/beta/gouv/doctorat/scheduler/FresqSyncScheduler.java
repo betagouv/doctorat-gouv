ceff8e9d81942ba132620828cb026fbdadf06001
mohamed-beta-dev <mohamed-laarbi.el-hani.ext@beta.gouv.fr>
2026-07-16 09:27:35 +0200

    feat: désactivation du scheduler Fresq par défaut

--- doctorat-gouv-backend/src/main/java/fr/dinum/beta/gouv/doctorat/scheduler/FresqSyncScheduler.java
+++ doctorat-gouv-backend/src/main/java/fr/dinum/beta/gouv/doctorat/scheduler/FresqSyncScheduler.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ import fr.dinum.beta.gouv.doctorat.repository.EcoleDoctoraleRepository;
 import fr.dinum.beta.gouv.doctorat.service.FresqApiService;
 
 @Component
+@ConditionalOnProperty(name = "fresq.scheduler.enabled", havingValue = "true", matchIfMissing = true)
 public class FresqSyncScheduler {
 	
 	private static final Logger log = LoggerFactory.getLogger(FresqSyncScheduler.class);
