b7ecb40b35cfa2ed47d2ccb1b791eaa67dddf39a
mohamed-beta-dev <mohamed-laarbi.el-hani.ext@beta.gouv.fr>
2026-08-14 10:34:17 +0200

    chore: mise a jour des versions pour la release 0.3.9

--- doctorat-gouv-frontend/src/environments/environment.prod.ts
+++ doctorat-gouv-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.3.9-beta',
+  version: '0.3.9',
   matomoSiteId: 249,
   apiUrl: '/api'
   
--- doctorat-gouv-frontend/src/environments/environment.ts
+++ doctorat-gouv-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  version: '0.3.9-beta',
+  version: '0.3.9',
   matomoSiteId: 249,
   apiUrl: 'http://localhost:8080/api' // URL de dev
 };
